96755d4fb66ef61737241237edb29d4f5172a672
oscarcanavan <canavanoscar@gmail.com>
2026-06-07 14:10:00 +0100

    fixed dodgy if syntax

--- moneyCase.sh
+++ moneyCase.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 
+# This is a random project I will use to practice my bash and German language
+
+
 echo "Wie viel Geld hast du?"
 read -r bar;
 #echo $bar
 
-if $bar < 10
+if [ $bar -le 10 ]
 then
   echo "wenn"
+else
+  echo "testing my script"
 fi
 
 case $bar in
